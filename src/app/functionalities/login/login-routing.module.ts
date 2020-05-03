cd639e12d9c0d6bc0d0cbc056dbca8feac30d4a1
Bruno Augusto <bruno.sidf@gmail.com>
2020-05-03 00:42:34 -0300

    Correção import

--- src/app/functionalities/login/login-routing.module.ts
+++ src/app/functionalities/login/login-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginPage } from './components/login/login.page';
-import { LoginContainerPage } from './Login-container.page';
+import { LoginContainerPage } from './login-container.page';
 
 const routes: Routes = [
   {
--- src/app/functionalities/login/login.module.ts
+++ src/app/functionalities/login/login.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
+import { NgxMaskIonicModule } from 'ngx-mask-ionic';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { ConfirmationPage } from './components/confirmation/confirmation.page';
+import { LoginPage } from './components/login/login.page';
 import { LoginEffects } from './effects/login.effects';
 import { LoginPageRoutingModule } from './login-routing.module';
-import { LoginPage } from './components/login/login.page';
-import { LoginContainerPage } from './login-container.page';
 import { featureKey, reducer } from './reducers';
-import { NgxMaskIonicModule } from 'ngx-mask-ionic';
-import { ConfirmationPage } from './components/confirmation/confirmation.page';
+import { LoginContainerPage } from './login-container.page';
 
 @NgModule({
   imports: [
